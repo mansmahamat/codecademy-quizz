e90291ab8863afb8cd4c9f6e23da3edad63d033f
mansmahamat <mansour.mahamat.salle@gmail.com>
2024-04-09 16:26:55 +0200

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Mansour Mahamat-salle on 09/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
